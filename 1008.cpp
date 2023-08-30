61ae762284cdb2429518287ba73f58106b52f836
myoungin <myoungin729@gmail.com>
2023-08-31 01:38:35 +0900

    1. 입출력과 사칙연산

---
+++ 1008.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+    cout.precision(10);
+    cout << (double)a / b;
+}
---
+++ 2588.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+    cout << a * (b%10) << "\n";
+    cout << a * ((b/10)%10) << "\n";
+    cout << a * (b/100) << "\n";
+    cout << a * b;
+}
